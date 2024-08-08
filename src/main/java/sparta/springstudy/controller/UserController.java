c480ad3bbe23fd7e801f6462a2e87bae81d919a3
yoon hyeon soo <chris24da@naver.com>
2024-08-08 18:43:45 +0900

    회원가입 구현

---
+++ src/main/java/sparta/springstudy/controller/UserController.java
@@ -0,0 +1,36 @@
+package sparta.springstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sparta.springstudy.dto.SignupRequestDto;
+import sparta.springstudy.service.UserService;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signUp(SignupRequestDto requestDto){
+        userService.signup(requestDto);
+        return "redirect:/api/user/login-page";
+    }
+}
---
+++ src/main/java/sparta/springstudy/entity/User.java
@@ -0,0 +1,40 @@
+package sparta.springstudy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)//enum 타입을 저장할 어노테이션
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
+
+
+
